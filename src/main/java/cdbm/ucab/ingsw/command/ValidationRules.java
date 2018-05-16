d49b8547ffe32e1d7c25eec76236c28677d2d479
trekexol <39245161+trekexol@users.noreply.github.com>
2018-05-15 20:40:18 -0400

    Update ValidationRules.java

--- src/main/java/cdbm/ucab/ingsw/command/ValidationRules.java
+++ src/main/java/cdbm/ucab/ingsw/command/ValidationRules.java
@@ -4,5 +4,5 @@ public interface ValidationRules {
     int EMAIL_MIN_SIZE = 12;
     int PASSWORD_MIN_SIZE = 8;
     int FIRST_LAST_NAME_MAX_SIZE = 40;
-    String FIRST_LAST_NAME_REGEX = "[a-zA-Z ]([&.'-]?[a-zA-Z ][&.'-]?)*";
+    String FIRST_LAST_NAME_REGEX = "[a-zA-Z ]([^&]?[a-zA-Z ][^&]?)*";
 }
